62bccad2d0e8d9d9deb7f2fe0db245635c2417df
Allen Tomlinson <k0reanach0@gmail.com>
2015-05-08 16:00:27 -0500

    edmond food, initial commit

---
+++ examples/data/places.js
@@ -0,0 +1,268 @@
+var food = [
+  {
+    "name": "Subway",
+    "mood": "Sandwiches",
+    "cost": 5,
+    "dist": "< 1 mi.",
+    "type": "Takeout"
+  },
+  {
+    "name": "Arby's",
+    "mood": "Sandwiches",
+    "cost": 5,
+    "dist": "< 1 mi.",
+    "type": "Takeout"
+  },
+  {
+    "name": "Whataburger",
+    "mood": "Burgers",
+    "cost": 5,
+    "dist": "< 1 mi.",
+    "type": "Takeout"
+  },
+  {
+    "name": "Long John Silver's",
+    "mood": "Seafood",
+    "cost": 5,
+    "dist": "< 1 mi.",
+    "type": "Takeout"
+  },
+  {
+    "name": "Chipotle",
+    "mood": "Mexican",
+    "cost": 10,
+    "dist": "< 2 mi.",
+    "type": "Takeout"
+  },
+  {
+    "name": "Raising Cane's",
+    "mood": "Chicken",
+    "cost": 5,
+    "dist": "< 4 mi.",
+    "type": "Takeout"
+  },
+  {
+    "name": "Chick-fil-a",
+    "mood": "Chicken",
+    "cost": 5,
+    "dist": "< 3 mi.",
+    "type": "Takeout"
+  },
+  {
+    "name": "Panda Express",
+    "mood": "Asian",
+    "cost": 5,
+    "dist": "< 5 mi.",
+    "type": "Takeout"
+  },
+  {
+    "name": "Pei Wei",
+    "mood": "Asian",
+    "cost": 10,
+    "dist": "< 4 mi.",
+    "type": "Takeout"
+  },
+  {
+    "name": "Sonic",
+    "mood": "Burger",
+    "cost": 5,
+    "dist": "< 3 mi.",
+    "type": "Takeout"
+  },
+  {
+    "name": "Braum's",
+    "mood": "Burgers",
+    "cost": 5,
+    "dist": "< 3 mi.",
+    "type": "Takeout"
+  },
+  {
+    "name": "Popeye's",
+    "mood": "Chicken",
+    "cost": 5,
+    "dist": "< 3 mi.",
+    "type": "Takeout"
+  },
+  {
+    "name": "McDonald's",
+    "mood": "Burgers",
+    "cost": 5,
+    "dist": "< 4 mi.",
+    "type": "Takeout"
+  },
+  {
+    "name": "Nhinja",
+    "mood": "Sushi",
+    "cost": 15,
+    "dist": "< 3 mi.",
+    "type": "Restaurant"
+  },
+  {
+    "name": "Phobulous",
+    "mood": "Noodles",
+    "cost": 10,
+    "dist": "< 4 mi.",
+    "type": "Restaurant"
+  },
+  {
+    "name": "Hu-Hot",
+    "mood": "Noodles",
+    "cost": 10,
+    "dist": "< 6 mi.",
+    "type": "Restaurant"
+  },
+  {
+    "name": "Alfredo's",
+    "mood": "Mexican",
+    "cost": 10,
+    "dist": "< 5 mi.",
+    "type": "Restaurant"
+  },
+  {
+    "name": "SUMO",
+    "mood": "Asian",
+    "cost": 50,
+    "dist": "< 3 mi.",
+    "type": "Restaurant"
+  },
+  {
+    "name": "Old Chicago",
+    "mood": "Pizza",
+    "cost": 10,
+    "dist": "< 4 mi.",
+    "type": "Restaurant"
+  },
+  {
+    "name": "Pepe's",
+    "mood": "Mexican",
+    "cost": 10,
+    "dist": "< 4 mi.",
+    "type": "Restaurant"
+  },
+  {
+    "name": "Earl's",
+    "mood": "BBQ",
+    "cost": 10,
+    "dist": "< 4 mi.",
+    "type": "Takeout"
+  },
+  {
+    "name": "Billy Sims",
+    "mood": "BBQ",
+    "cost": 10,
+    "dist": "< 4 mi.",
+    "type": "Takeout"
+  },
+  {
+    "name": "Stars & Stripes",
+    "mood": "Pizza",
+    "cost": 5,
+    "dist": "< 3 mi.",
+    "type": "Takeout"
+  },
+  {
+    "name": "Taco Bueno",
+    "mood": "Mexican",
+    "cost": 5,
+    "dist": "< 1 mi.",
+    "type": "Takeout"
+  },
+  {
+    "name": "Papa John's",
+    "mood": "Pizza",
+    "cost": 10,
+    "dist": "< 1 mi.",
+    "type": "Takeout"
+  },
+  {
+    "name": "Taco Bell",
+    "mood": "Mexican",
+    "cost": 5,
+    "dist": "< 5 mi.",
+    "type": "Takeout"
+  },
+  {
+    "name": "Burger King",
+    "mood": "Burgers",
+    "cost": 5,
+    "dist": "< 5 mi.",
+    "type": "Takeout"
+  },
+  {
+    "name": "The Garage",
+    "mood": "Burgers",
+    "cost": 10,
+    "dist": "< 5 mi.",
+    "type": "Takeout"
+  },
+  {
+    "name": "Planet Sub",
+    "mood": "Sandwiches",
+    "cost": 10,
+    "dist": "< 5 mi.",
+    "type": "Takeout"
+  },
+  {
+    "name": "Wahaha Express",
+    "mood": "Asian",
+    "cost": 10,
+    "dist": "< 1 mi.",
+    "type": "Takeout"
+  },
+  {
+    "name": "Cafe 7",
+    "mood": "Italian",
+    "cost": 10,
+    "dist": "< 7 mi.",
+    "type": "Takeout"
+  },
+  {
+    "name": "Hideaway Pizza",
+    "mood": "Pizza",
+    "cost": 10,
+    "dist": "< 1 mi.",
+    "type": "Takeout"
+  },
+  {
+    "name": "Mazzio's",
+    "mood": "Pizza",
+    "cost": 10,
+    "dist": "< 2 mi.",
+    "type": "Takeout"
+  },
+  {
+    "name": "Which Wich",
+    "mood": "Sandwiches",
+    "cost": 10,
+    "dist": "< 5 mi.",
+    "type": "Takeout"
+  },
+  {
+    "name": "Jersey Mike's",
+    "mood": "Sandwiches",
+    "cost": 10,
+    "dist": "< 3 mi.",
+    "type": "Takeout"
+  },
+  {
+    "name": "Firehouse Subs",
+    "mood": "Sandwiches",
+    "cost": 10,
+    "dist": "< 3 mi.",
+    "type": "Takeout"
+  },
+  {
+    "name": "Zarate's",
+    "mood": "Mediterranean",
+    "cost": 15,
+    "dist": "< 1 mi.",
+    "type": "Restaurant"
+  },
+  {
+    "name": "Bighead's",
+    "mood": "Cajun",
+    "cost": 10,
+    "dist": "< 1 mi.",
+    "type": "Takeout"
+  }
+];
